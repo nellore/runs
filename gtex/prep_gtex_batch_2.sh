c0266d568f3617ce696db50b10484a1fe66e00b1
Abhinav Nellore <anellore@gmail.com>
2015-11-21 20:50:24 -0500

    writes all gtex scripts

---
+++ gtex/prep_gtex_batch_2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rail-rna prep elastic -m $DIR/gtex_batch_2.manifest --profile dbgap --secure-stack-name dbgap --dbgap-key /Users/eterna/gtex/prj_8716.ngc --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge -o s3://rail-dbgap/gtex_prep_batch_2 -c 63 --core-instance-bid-price 0.25 --master-instance-bid-price 0.25
