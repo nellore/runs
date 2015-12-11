b2036150b849b8e05eb6396091af0abb79d5f9af
lcolladotor <lcollado@jhsph.edu>
2015-12-10 19:04:06 -0500

    Try a different approach to minimize input/output

--- gtex/DER_analysis/coverageMatrix/region_cuts/find_region_cuts.R
+++ gtex/DER_analysis/coverageMatrix/region_cuts/find_region_cuts.R
@@ -11,35 +11,27 @@ cuts <- seq(from = 0.1, to = 5, by = 0.1)
 ## Parallel environment to use
 bp <- SnowParam(workers = 10, outfile = Sys.getenv('SGE_STDERR_PATH'))
 
-## Load the mean coverage for all the chromosomes
-meanCovs <- bplapply(chrs, function(chr) {
-    library('derfinder')
-    message(paste(Sys.time(), 'processing', chr))
-    meanCov <- loadCoverage('/dcl01/leek/data/gtex_work/gtex_mean_coverage.bw', chr)
-    file.path <- file.path(Sys.getenv('TMPDIR'), paste0(chr, '-meanCov.Rdata'))
-    message(paste(Sys.time(), 'saving meanCov for', chr, 'at', file.path))
-    save(meanCov, file = file.path)
-    return(file.path)
-}, BPPARAM = bp)
-names(meanCovsFiles) <- chrs
-#save(meanCovs, file = 'meanCovs.Rdata')
-#> print(object.size(meanCovs), units = 'Gb')
-#27.9 Gb
-
 ## Find the regions for all the chromosomes given a specific cutoff
-getRegs <- function(chr, meanCovFile, cutoff) {
+getRegChr <- function(cutoff, chr, meanCov) {
     library('derfinder')
     library('IRanges')
-    message(paste(Sys.time(), 'loading meanCov file', meanCovFile))
-    load(meanCovFile)
     message(paste(Sys.time(), 'processing', chr, 'with cutoff', cutoff))
     regs <- findRegions(position = Rle(TRUE, length(meanCov$coverage[[1]])), fstats = meanCov$coverage[[1]], chr = chr, maxClusterGap = 300L, cutoff = cutoff, verbose = FALSE)
     return(regs)
 }
+getRegs <- function(chr, cutoffs, param) {
+    message(paste(Sys.time(), 'loading meanCov for', chr))
+    meanCov <- loadCoverage('/dcl01/leek/data/gtex_work/gtex_mean_coverage.bw', chr)
+    res <- bplapply(cutoffs, getRegChr, chr = chr, meanCov = meanCov, BPPARAM = param)
+    names(res) <- cutoffs
+    return(res)
+}
 
-region_cuts <- lapply(cuts, function(cutoff) {
-    message(paste(Sys.time(), 'working with cutoff', cutoff))
-    regs <- bpmapply(getRegs, chrs, meanCovsFiles, MoreArgs = list(cutoff = cutoff), BPPARAM = bp)
+region_cuts_raw <- lapply(chrs, getRegs, cutoffs = cuts, param = bp)
+names(region_cuts_raw) <- chrs
+
+region_cuts <- lapply(cuts, function(cutoff, chrs = chrs) {
+    regs <- lapply(chrs, function(chr) { region_cuts_raw[[chr]][[cutoff]] })
     regs <- unlist(GRangesList(regs))
     return(regs)
 })
