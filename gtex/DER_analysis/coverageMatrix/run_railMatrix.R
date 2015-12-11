981c2d5bf79a710e852fb19656eedcce25e4e118
lcolladotor <lcollado@jhsph.edu>
2015-12-11 12:51:01 -0500

    Switch strategy again. I was not seeing a faster run time for chrY with 30 cores vs 5 or 4 cores. So, this time I'll try with just 1 core but lots of memory for the latter steps: filterData(), getRegionCoverage()

--- gtex/DER_analysis/coverageMatrix/run_railMatrix.R
+++ gtex/DER_analysis/coverageMatrix/run_railMatrix.R
@@ -45,7 +45,7 @@ map <- match(gsub('/dcl01/leek/data/gtex/batch_[0-9]*/coverage_bigwigs/|.bw', ''
 counts <- counts[map, ]
 
 ## Run railMatrix
-regionMat <- railMatrix(chrs, summaryFiles, sampleFiles, L = pheno$avgLength / 2, cutoff = cutoff, targetSize = 40e6, totalMapped = counts$totalMapped, file.cores = 30, returnBP = FALSE)
+regionMat <- railMatrix(chrs, summaryFiles, sampleFiles, L = pheno$avgLength / 2, cutoff = cutoff, targetSize = 40e6, totalMapped = counts$totalMapped, file.cores = 1L, returnBP = FALSE)
 
 ## Save results
 save(regionMat, file=paste0('regionMat-cut', cutoff, '-chr', opt$chrnum, '.Rdata'))
