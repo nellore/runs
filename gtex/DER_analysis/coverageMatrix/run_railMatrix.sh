eec0877250ceb688715c6a113cf17ff883d8acb9
lcolladotor <lcollado@jhsph.edu>
2015-12-13 01:36:41 -0500

    Test derfinder v1.5.15 to see if it uses less mem now with cutoff = 3
    If it helps reduce the memory load, then we might be able to move ahead with
    cutoff = 0.5. Anyhow, I just want to know if the memory load is reduced with
    using for() loops instead of mapply() and the expensive matrix for dividing by
    'L'.

--- gtex/DER_analysis/coverageMatrix/run_railMatrix.sh
+++ gtex/DER_analysis/coverageMatrix/run_railMatrix.sh
@@ -20,7 +20,7 @@ for chrnum in Y X 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
 #!/bin/bash
 #$ -cwd
 #$ -m e
-#$ -l mem_free=250G,h_vmem=300G,h_fsize=100G
+#$ -l mem_free=300G,h_vmem=350G,h_fsize=100G
 #$ -N ${sname}
 
 echo '**** Job starts ****'
