1c9d65a425e4c7a3a649142b8959ae42043c5279
Abhinav Nellore <anellore@gmail.com>
2015-12-14 14:36:28 -0500

    fixes mean coverage script

--- gtex/mean.sh
+++ gtex/mean.sh
@@ -7,12 +7,12 @@
 BWTOOL=$1
 BED=$2
 export BW=$3
-RUNINFO=$4
+export RUNINFO=$4
 DUMP=$5
 mkdir -p $DUMP
-export SAMPLENAME=$(echo $(basename $BW) | rev | awk '{print substr($0, 4)}')
+export SAMPLENAME=$(echo $(basename $BW) | rev | awk '{print substr($0, 4)}' | rev)
 SRR=$(echo $SAMPLENAME | cut -d'_' -f1)
-COUNTS=$(dirname $(dirname $BW))/cross_sample_results/counts.tsv.gz
+export COUNTS=$(dirname $(dirname $BW))/cross_sample_results/counts.tsv.gz
 READLENGTH=$(grep $SRR $RUNINFO | cut -d',' -f9)
 READCOUNT=$(gzip -cd $COUNTS | grep $SAMPLENAME | rev | cut -f1 | rev | cut -d',' -f1)
-$BWTOOL summary $BED $BW /dev/stdout | cut -f1-3,8 | awk -v rcount=$READCOUNT -v rlength=$READLENGTH '{print $1 "\t" $2 "\t" $3 "\t" $4/$rcount*40000000/$rlength*2}' >$DUMP/$SRR.mean.tsv
+$BWTOOL summary $BED $BW /dev/stdout | cut -f1-3,8 | awk -v rcount=$READCOUNT -v rlength=$READLENGTH '{print $1 "\t" $2 "\t" $3 "\t" $4*40000000*2/(rcount*rlength)}' >$DUMP/$SRR.mean.tsv
