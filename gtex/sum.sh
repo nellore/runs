337cfd6d4caaea74b28f09957ba671a47244eec5
Abhinav Nellore <anellore@gmail.com>
2015-12-15 14:25:20 -0500

    fixes bugs in coverage sum scripts; refactors so mean -> sum

--- gtex/sum.sh
+++ gtex/sum.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# Helper script that uses bwtool to generate mean coverages in regions from bed file
+# Helper script that uses bwtool to generate coverage sums in regions from bed file
 # $1: path to bwtool 1.0; download it from https://github.com/CRG-Barcelona/bwtool/releases/tag/1.0
 # $2: path to BED file with regions
 # $3: path to bigWig file in GTEx output directory; assumes directory structure of output directory in download.sh
 # $4: path to SraRunInfo.csv from GTEx repository
-# $5: path to directory in which to dump mean coverage
+# $5: path to directory in which to dump coverage sums
 BWTOOL=$1
 BED=$2
 export BW=$3
@@ -15,5 +15,5 @@ export SAMPLENAME=$(echo $(basename $BW) | rev | awk '{print substr($0, 4)}' | r
 SRR=$(echo $SAMPLENAME | cut -d'_' -f1)
 export COUNTS=$(dirname $(dirname $BW))/cross_sample_results/counts.tsv.gz
 READLENGTH=$(grep $SRR $RUNINFO | cut -d',' -f9)
-READCOUNT=$(gzip -cd $COUNTS | grep $SAMPLENAME | rev | cut -f1 | rev | cut -d',' -f1)
-$BWTOOL summary $BED $BW /dev/stdout -fill=0 | cut -f1-3,8 | awk -v rcount=$READCOUNT -v rlength=$READLENGTH '{print $1 "\t" $2 "\t" $3 "\t" $4*40000000*2/(rcount*rlength)}' >$DUMP/$SRR.mean.tsv
+READCOUNT=$(gzip -cd $COUNTS | grep $SAMPLENAME | rev | cut -f2 | rev | cut -d',' -f1)
+$BWTOOL summary $BED $BW /dev/stdout -fill=0 -with-sum | cut -f1-3,10 | awk -v rcount=$READCOUNT -v rlength=$READLENGTH '{print $1 "\t" $2 "\t" $3 "\t" $4*40000000*2/(rcount*rlength)}' >$DUMP/$SRR.sum.tsv
