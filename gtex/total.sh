7eb7099e57afea63bb8c393c62e2e095e5863398
Abhinav Nellore <anellore@gmail.com>
2015-12-18 17:30:45 -0500

    adds script for obtained total number of reads aligned

---
+++ gtex/total.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Computes total number of mates aligned from GTEx output data
+# $1: GTEx output directory from download.sh
+GTEX=$1
+for i in $GTEX/*; do gzip -cd $i/cross_sample_results/counts.tsv.gz | tail -n +2 | rev | cut -f1 | rev | cut -d "," -f1 | paste -s -d+ | bc; done | paste -s -d+ | bc
