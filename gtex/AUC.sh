35f3ecd2de1388474c078607eae70ebe0362af83
Abhinav Nellore <anellore@gmail.com>
2015-12-24 17:57:29 -0500

    gets basename

--- gtex/AUC.sh
+++ gtex/AUC.sh
@@ -26,7 +26,7 @@ mkdir -p $OUT/tempaucs
 cd $OUT/tempaucs
 for bigwig in $(for i in $INPUT/*; do find $i/coverage_bigwigs -name \*.bw | grep -Ev 'mean|median|unique'; done)
 do
-	$WIGGLETOOLS AUC $bigwig >$($(basename $bigwig) | cut -d'_' -f1) &
+	$WIGGLETOOLS AUC $bigwig >$(basename $bigwig | cut -d'_' -f1) &
 	nrwait $PROCESSES
 done
 wait
