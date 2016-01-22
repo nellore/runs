9acd0ddd09934d3df52de5d4366aab2cc62988be
Abhinav Nellore <anellore@gmail.com>
2016-01-22 15:22:28 -0500

    fixes a coordinate

--- sra/alk.sh
+++ sra/alk.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # Compressed junctions file (intropolis.v1.tsv.gz) should be first command-line parameter
-# According to http://grch37.ensembl.org/Homo_sapiens/Transcript/Exons?db=core;g=ENSG00000171094;r=2:29415640-30144432;t=ENST00000389048
-# the ALK gene is on "Chromosome 2: 29,415,640-30,144,432 reverse strand."
+# According to mysql  -h  genome-mysql.cse.ucsc.edu -A -u genome -D hg19 -e 'select * from refGene where name="NM_004304"\G'
+# the ALK gene is on "Chromosome 2: 29,415,640-30,144,478 reverse strand."
+# (Note we use 1-based coordinates while 0-based coordinates are returned by the mysql command
 JUNC=$1
 # Select junctions only in ALK
-gzip -cd $JUNC | grep -w chr2 | grep -w "-" | awk '$2 >= 29415640 && $2 <= 30144432 && $3 >= 29415640 && $3 <= 30144432' | gzip >alk_junctions.tsv.gz
+gzip -cd $JUNC | grep -w chr2 | grep -w "-" | awk '$2 >= 29415640 && $2 <= 30144478 && $3 >= 29415640 && $3 <= 30144478' | gzip >alk_junctions.tsv.gz
 # Now according to the Nature paper http://www.nature.com/nature/journal/v526/n7573/full/nature15258.html,
 # many cancers have an alternative transcription initiation site after intron 19; in other words,
 # for the so-called alternative ALK^{ATI} transcript, no junction with coordinates >= 29,446,395
