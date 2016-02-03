cc68f8e81f6c72613f0fba9531a45e0a42ec714b
Abhinav Nellore <anellore@gmail.com>
2016-02-03 17:06:23 -0500

    preps for sra rerun

---
+++ sra/v2/hg38/prep_sra_batch_59.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rail-rna prep elastic -m $DIR/sra_batch_59.manifest --skip-bad-records --core-instance-type m3.xlarge --master-instance-type m3.xlarge -o s3://rail-sra-hg38/sra_prep_batch_59 -c 20 --core-instance-bid-price 0.25 --master-instance-bid-price 0.25 -f --max-task-attempts 6
