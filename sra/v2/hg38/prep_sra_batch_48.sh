1e8ade177c9408a1cf689f116b83f63cb80896f4
Abhinav Nellore <anellore@gmail.com>
2016-02-14 13:55:53 -0500

    adds ec2 key name

--- sra/v2/hg38/prep_sra_batch_48.sh
+++ sra/v2/hg38/prep_sra_batch_48.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rail-rna prep elastic -m $DIR/sra_batch_48.manifest --skip-bad-records --ignore-missing-sra-samples --core-instance-type m3.xlarge --master-instance-type m3.xlarge -o s3://rail-sra-hg38/sra_prep_batch_48 -c 20 --core-instance-bid-price 0.25 --master-instance-bid-price 0.25 -f --max-task-attempts 6 --region us-east-1
+rail-rna prep elastic -m $DIR/sra_batch_48.manifest --skip-bad-records --ignore-missing-sra-samples --core-instance-type m3.xlarge --master-instance-type m3.xlarge -o s3://rail-sra-hg38/sra_prep_batch_48 -c 20 --core-instance-bid-price 0.25 --master-instance-bid-price 0.25 -f --max-task-attempts 6 --region us-east-1 --ec2-key-name useast1
