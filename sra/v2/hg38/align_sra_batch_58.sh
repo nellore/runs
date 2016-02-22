d8dd9ae05a8ad0bb805baee058890a7b1223b936
Abhinav Nellore <anellore@gmail.com>
2016-02-21 20:30:30 -0500

    preps for another go at all of sra

--- sra/v2/hg38/align_sra_batch_58.sh
+++ sra/v2/hg38/align_sra_batch_58.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rail-rna align elastic -m $DIR/sra_batch_58.manifest --core-instance-type c3.8xlarge --master-instance-type c3.8xlarge -c 80 -e --core-instance-bid-price 1.7 --master-instance-bid-price 1.7 -i s3://rail-sra-hg38/sra_prep_batch_58 -o s3://rail-sra-hg38/sra_align_batch_58 -a hg38 -f -d jx,tsv,bed,bw,idx --max-task-attempts 6 --region us-east-1 --ec2-key-name useast1
+rail-rna align elastic -m $DIR/sra_batch_58.manifest --core-instance-type c3.8xlarge --master-instance-type c3.8xlarge --junction-criteria .01,-1 -c 80 -e --core-instance-bid-price 1.7 --master-instance-bid-price 1.7 -i s3://rail-sra-hg38/sra_prep_batch_58 -o s3://rail-sra-hg38/sra_results_batch_58 -a hg38 -f -d jx,tsv,bed,bw,idx --max-task-attempts 6 --region us-east-1 --ec2-key-name useast1
