edf240bfcbe8e46b1ec3d41c080553a14edf6494
Abhinav Nellore <anellore@gmail.com>
2016-03-15 14:08:41 -0400

    fixes filename

--- sra/v2/hg38/get_biosample_data.sh
+++ sra/v2/hg38/get_biosample_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Creates a table with sample metadata to facilitate manual construction of a tidier table.
 # Requires curl, php, and intropolis.idmap.v2.hg38.tsv.gz
-for samp in $(cut -f3 ../intropolis.idmap.v2.hg38.tsv.gz | sort | uniq); do curl "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=biosample&id=$(curl "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=biosample&term=$samp" | sed -n 's|[^<]*<Id>\([^<]*\)</Id>[^<]*|\1|gp')" | grep '<SampleData>' | php -r 'while(($line=fgets(STDIN)) !== FALSE) echo html_entity_decode($line, ENT_QUOTES|ENT_HTML401);' | awk -v mysamp=$samp '{print mysamp "\t" $0}'; done >biosample_data_to_parse.tsv
+for samp in $(cut -f3 ../intropolis.idmap.v2.hg38.tsv | sort | uniq); do curl "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=biosample&id=$(curl "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=biosample&term=$samp" | sed -n 's|[^<]*<Id>\([^<]*\)</Id>[^<]*|\1|gp')" | grep '<SampleData>' | php -r 'while(($line=fgets(STDIN)) !== FALSE) echo html_entity_decode($line, ENT_QUOTES|ENT_HTML401);' | awk -v mysamp=$samp '{print mysamp "\t" $0}'; done >biosample_data_to_parse.tsv
 cat biosample_data_to_parse.tsv | python xmlparse.py >parsed_biosample_data.tsv
 cat parsed_biosample_data.tsv | python tag.py >biosample_tags.tsv
 #cat parsed_biosample_data.tsv | python classify_cell_line.py
