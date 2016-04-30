43704d97f525b66035fee6f39d9650ce8bbfe656
lcolladotor <lcollado@jhsph.edu>
2016-04-30 09:00:19 -0400

    Check tsv files

---
+++ sra/DER_analysis/coverageMatrix/ers_gtex/run_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -cwd
+#$ -m e
+#$ -l mem_free=2G,h_vmem=3G
+#$ -pe local 25
+#$ -N ers_gtex-check-tsv
+#$ -hold_jid sra_ers_gtex_bwtool
+
+echo '**** Job starts ****'
+date
+
+## Make logs dir
+mkdir -p logs
+
+## Annotate regions
+module load R/3.3
+Rscript check_tsv.R
+
+## Move log files into the logs directory
+mv check-tsv.* logs/
+
+echo '**** Job ends ****'
+date
