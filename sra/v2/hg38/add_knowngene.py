c90b37e75d3fb9e86e7de9d4f3c0b76ef539ef70
Abhinav Nellore <anellore@gmail.com>
2016-06-16 13:59:14 -0400

    adds transcripts in which donor/acceptor are present

--- sra/v2/hg38/add_knowngene.py
+++ sra/v2/hg38/add_knowngene.py
@@ -9,7 +9,8 @@ corresponding gene IDs.
 Requires that GTF has been downloaded via the UCSC Table Browser at 
 http://genome.ucsc.edu/cgi-bin/hgTables?command=start ; we downloaded this GTF
 on June 13, 2016 as knownGene_hg38.gtf.gz, which is in the same dir as this
-script.
+script. ALSO CORRECTS JUNCTION COORDINATES FROM junctions_by_project.py so
+they're zero-based!
 """
 from collections import defaultdict
 import gzip
@@ -45,12 +46,15 @@ if __name__ == '__main__':
             attribute[id_index] = attribute[id_index].strip()
             quote_index = attribute[id_index].index('"')
             exons[attribute[id_index][quote_index+1:-1]].add(
-                    (tokens[0], int(tokens[3]), int(tokens[4]))
+                    (tokens[0], int(tokens[3]), int(tokens[4]), tokens[6])
                 )
 
     junctions = defaultdict(list)
+    donors = defaultdict(list)
+    acceptors = defaultdict(list)
     for transcript_id in exons:
         exons_from_transcript = sorted(list(exons[transcript_id]))
+        strand = exons_from_transcript[0][3]
         # Recall that GTF is end-inclusive, and so is STAR's junctions.txt
         for i in xrange(1, len(exons_from_transcript)):
             if exons_from_transcript[i][0] == exons_from_transcript[i-1][0]:
@@ -59,30 +63,53 @@ if __name__ == '__main__':
                     - exons_from_transcript[i-1][2]) < 5:
                     continue
                 junction = (exons_from_transcript[i][0],
-                            exons_from_transcript[i-1][2] + 1,
-                            exons_from_transcript[i][1] - 1)
+                            exons_from_transcript[i-1][2],
+                            exons_from_transcript[i][1] - 2)
+                donor = (exons_from_transcript[i][0],
+                         exons_from_transcript[i-1][2])
+                acceptor = (exons_from_transcript[i][0],
+                            exons_from_transcript[i][1] - 2)
+                if strand == '-':
+                    donor, acceptor = acceptor, donor
+                donors[donor].append(transcript_id)
+                acceptors[acceptor].append(transcript_id)
                 junctions[junction].append(transcript_id)
 
+    # Postprocess
+    junctions_write = defaultdict(lambda: 'NA')
+    donors_write = defaultdict(lambda: 'NA')
+    acceptors_write = defaultdict(lambda: 'NA')
+    for junction in junctions:
+        junctions_write[junction] = ';'.join(junctions[junction])
+    for donor in donors:
+        donors_write[donor] = ';'.join(donors[donor])
+    for acceptor in acceptors:
+        acceptors_write[acceptor] = ';'.join(acceptors[acceptor])
     # Modify BED files
     for bed in args.beds:
         assert bed.endswith('.bed.gz'), (
                 '{} does not have extension ".bed.gz".'.format(bed)
             )
-        modified_bed_filename = bed[:-7] + '_with_transcript.bed.gz'
+        modified_bed_filename = bed[:-7] + '_with_transcripts.bed.gz'
         with gzip.open(modified_bed_filename, 'w') as output_stream, gzip.open(
                 bed
             ) as input_stream:
             for line in input_stream:
                 tokens = line.strip().split('\t')
-                junction = (tokens[0], int(tokens[1]), int(tokens[2]))
-                if junction in junctions:
-                    print >>output_stream, '\t'.join(
+                junction = (tokens[0], int(tokens[1]) - 1, int(tokens[2]) - 1)
+                strand = tokens[5]
+                donor = junction[:2]
+                acceptor = (junction[0], junction[2])
+                if strand == '-':
+                    donor, acceptor = acceptor, donor
+                print >>output_stream, '\t'.join(
                             tokens[:3]
-                            + [tokens[3] + ':' + ';'.join(
-                                    [str(el) for el in junctions[junction]]
-                                )] + tokens[4:]
-                        )
-                else:
-                    print >>output_stream, '\t'.join(
-                            tokens[:3] + [tokens[3] + ':NA'] + tokens[4:]
+                            + [''.join([tokens[3],
+                                        '|D:',
+                                        donors_write[donor],
+                                        '|A:',
+                                        acceptors_write[acceptor],
+                                        '|J:',
+                                        junctions_write[junction]])]
+                            + tokens[4:]
                         )
