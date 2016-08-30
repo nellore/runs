0d5e5335d3df9de432acb08f62d8143f4bb77db2
Abhinav Nellore <anellore@gmail.com>
2016-08-29 21:32:18 -0500

    adds translatome manifest and script

---
+++ sra/translatome.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Runs Rail-RNA on translatome samples from project http://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP031883
+# Use Rail-RNA v0.2.3b and mm10 iGenome from ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Mus_musculus/UCSC/mm10/Mus_musculus_UCSC_mm10.tar.gz
+# Specify path to Bowtie 1/2 index basenames for mm10 iGenome here
+MM10=
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rail-rna go local -m $DIR/translatome.manifest -x $MM10 -d jx
