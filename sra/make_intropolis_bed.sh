608454e2d0d3611dcd176c0fea4527c82e521a74
Abhinav Nellore <anellore@gmail.com>
2016-09-06 20:55:35 -0500

    finishes script for making intropolis bed

---
+++ sra/make_intropolis_bed.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Creates BED file with intropolis junctions
+# $1: location of intropolis.v1.hg19.tsv.gz
+# $2: destination directory
+# $3: location of bedToBigBed executable (obtained from http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/bedToBigBed)
+INTROPOLIS=$1
+DEST=$2
+BEDTOBIGBED=$3
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $DEST
+gzip -cd $INTROPOLIS | awk '{print $1 "\t" ($2 - 1) "\t" $3 "\tjunction_" NR "\t1000\t" $4}' | gzip >$DEST/intropolis.v1.hg19.bed.gz
+gzip -cd $DEST/intropolis.v1.hg19.bed.gz >intropolis.v1.hg19.bed
+$DIR/hg19.chrom.sizes
