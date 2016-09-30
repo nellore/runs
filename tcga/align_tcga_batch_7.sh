0a69a754ff13cce27e8a94f4dc1197ae373848b3
Abhinav Nellore <anellore@gmail.com>
2016-09-29 20:00:43 -0500

    adds all dummy manifest files

---
+++ tcga/align_tcga_batch_7.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rail-rna align elastic -m $DIR/tcga_batch_7.manifest --profile dbgap --secure-stack-name dbgap --core-instance-type c3.8xlarge --master-instance-type c3.8xlarge -c 80 --core-instance-bid-price 1.2 --master-instance-bid-price 1.2 -i s3://dummy-bucket/tcga_prep_batch_7 -o s3://dummy-bucket/tcga_align_batch_7 -a hg38 -f -d jx,tsv,bed,bw,idx --max-task-attempts 6
---
+++ tcga/prep_tcga_batch_13.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# based on http://stackoverflow.com/questions/4632028/how-to-create-a-temporary-directory
+WORKDIR=`mktemp -d -p "$DIR"`
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORKDIR"
+  echo "Deleted temp working directory $WORKDIR"
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+cat tcga_batch_13.manifest | python $DIR/true_manifest.py >$WORKDIR/tcga_batch_13.manifest
+rail-rna prep elastic -m $WORKDIR/tcga_batch_13.manifest --profile dbgap --secure-stack-name dbgap --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge -o s3://dummy-bucket/gtex_prep_batch_13 -c 63 --core-instance-bid-price 0.2 --master-instance-bid-price 0.2 -f --max-task-attempts 6
