733939001847e408f2fdc5af717fb1601260ef5c
Abhinav Nellore <anellore@gmail.com>
2016-10-15 08:45:26 -0500

    changes job flow names

--- tcga/prep_tcga_batch_9.sh
+++ tcga/prep_tcga_batch_9.sh
@@ -12,5 +12,5 @@ function cleanup {
 # register the cleanup function to be called on the EXIT signal
 trap cleanup EXIT
 cat tcga_batch_9.manifest | python $DIR/true_manifest.py --cgc-auth-token /Users/eterna/cgcauth.txt >$WORKDIR/tcga_batch_9.manifest
-rail-rna prep elastic -m $WORKDIR/tcga_batch_9.manifest --profile dbgap --secure-stack-name dbgap-us-east-1b --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge -o s3://sb-rail-rna-mapreduce/tcga_prep_batch_9 -c 48 --core-instance-bid-price 0.5 --master-instance-bid-price 0.5 -f --max-task-attempts 10 --skip-bad-records --do-not-check-manifest --name "TCGA prep batch 9 job flow"
+rail-rna prep elastic -m $WORKDIR/tcga_batch_9.manifest --profile dbgap --secure-stack-name dbgap-us-east-1b --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge -o s3://sb-rail-rna-mapreduce/tcga_prep_batch_9 -c 48 --core-instance-bid-price 0.5 --master-instance-bid-price 0.5 -f --max-task-attempts 10 --skip-bad-records --do-not-check-manifest --name TCGA_prep_batch_9_job_flow
 
