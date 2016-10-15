4ca8062abe0508789612ca3b651de707d3f9f06f
Abhinav Nellore <anellore@gmail.com>
2016-10-15 08:22:54 -0500

    fixes job flow naming

--- tcga/gen.py
+++ tcga/gen.py
@@ -154,7 +154,7 @@ rail-rna prep elastic -m $WORKDIR/{manifest_file} --profile dbgap \
 -o {s3_bucket}/tcga_prep_batch_{batch_number} \
 -c 48 --core-instance-bid-price {core_price} \
 --master-instance-bid-price {core_price} -f \
---max-task-attempts 10 --skip-bad-records --do-not-check-manifest
+--max-task-attempts 10 --skip-bad-records --do-not-check-manifest \
 --name "TCGA prep batch {batch_number} job flow"
 """
                 ).format(manifest_file='tcga_batch_{}.manifest'.format(i),
